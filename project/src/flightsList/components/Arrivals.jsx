cf90093ba8c750afc0718584454b48088763d804
Rmorhub <rmorphone@gmail.com>
2021-11-07 18:17:46 +0200

    d

--- project/src/flightsList/components/Arrivals.jsx
+++ project/src/flightsList/components/Arrivals.jsx
@@ -9,50 +9,36 @@ import { flightsListSelector } from '../flightsList.selectors';
 const Arrivals = ({ searchDataArrival, flightsList }) => {
   const flights = searchDataArrival ?? flightsList;
 
-  return (
-    <table className="flights">
-      <thead className="flights-nav">
-        <tr>
-          <th className="flights-nav_item">Термінал</th>
-          <th className="flights-nav_item">Розклад</th>
-          <th className="flights-nav_item">Напрямок</th>
-          <th className="flights-nav_item">Статус</th>
-          <th className="flights-nav_item">Авіакомпанія</th>
-          <th className="flights-nav_item">Рейс</th>
-        </tr>
-      </thead>
-      {flights.length ? (
-        flights.map(el => {
-          const { term, timeToStand, timeLandFact, status } = el;
-          const city = el['airportFromID.city'];
-          const airlineCompany = el.airline.en.name;
-          const flightNum = el.codeShareData[0].codeShare;
-          const logo = el.codeShareData[0].airline.en.logoSmallName;
+  return flights.length ? (
+    flights.map(el => {
+      const { term, timeToStand, timeLandFact, status } = el;
+      const city = el['airportFromID.city'];
+      const airlineCompany = el.airline.en.name;
+      const flightNum = el.codeShareData[0].codeShare;
+      const logo = el.codeShareData[0].airline.en.logoSmallName;
 
-          return (
-            <tbody key={el.ID} className="flight-list">
-              <tr>
-                <td className="flight-list_item">
-                  <span>{term}</span>
-                </td>
-                <td className="flight-list_item">{timeFormatter(timeToStand)}</td>
-                <td className="flight-list_item">{city}</td>
-                <td className="flight-list_item">{checkStatus(status, timeLandFact)}</td>
-                <td className="flight-list_item">
-                  <div className="flight-list_item-box">
-                    <img src={logo} alt={airlineCompany} className="flight-img" />
-                    <span>{airlineCompany}</span>
-                  </div>
-                </td>
-                <td className="flight-list_item">{flightNum}</td>
-              </tr>
-            </tbody>
-          );
-        })
-      ) : (
-        <NotFound />
-      )}
-    </table>
+      return (
+        <tbody key={el.ID} className="flight-list">
+          <tr>
+            <td className="flight-list_item">
+              <span>{term}</span>
+            </td>
+            <td className="flight-list_item">{timeFormatter(timeToStand)}</td>
+            <td className="flight-list_item">{city}</td>
+            <td className="flight-list_item">{checkStatus(status, timeLandFact)}</td>
+            <td className="flight-list_item">
+              <div className="flight-list_item-box">
+                <img src={logo} alt={airlineCompany} className="flight-img" />
+                <span>{airlineCompany}</span>
+              </div>
+            </td>
+            <td className="flight-list_item">{flightNum}</td>
+          </tr>
+        </tbody>
+      );
+    })
+  ) : (
+    <NotFound />
   );
 };
 
